540a06707c982b55e3b922ef4a086b77698b4beb
Samuel <samuelmotaramos@gmail.com>
2024-06-06 21:42:34 -0300

    Calculadora

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import calculadora.calculadora;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+     calculadora calculadora = new calculadora();
+
+     calculadora.somar(1,2);
+
+     calculadora.subtrair(2,3);
+
+     calculadora.multiplicar(2,3);
+
+     calculadora.dividir(2,3);
+
+     //Quanto um 50 representa um porcentual de 500
+
+
+
+
+
+
+    }
+
+}
---
+++ src/calculadora/calculadora.java
@@ -0,0 +1,36 @@
+package calculadora;
+
+public class calculadora {
+    public float somar(float a, float b) {
+        float resultado = a + b;
+        float resultado2 = a - b;
+        return resultado;
+
+    }
+
+    public float subtrair(float a, float b) {
+        float resultado = a - b;
+        float resultado2 = a + b;
+        return resultado;
+
+    }
+
+    public float multiplicar(float a, float b) {
+        float resultado = a * b;
+        float resultado2 = a / b;
+        return resultado;
+
+    }
+
+    public float dividir(float a, float b) {
+
+        float resultado = a / b;
+        float resultado2 = a % b;
+        return resultado;
+
+    }
+
+}
+
+
+
